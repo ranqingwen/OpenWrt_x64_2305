201bbfb1c4bbd6bf771373481da72ae414752083
gxnas <52808122+gxnas@users.noreply.github.com>
2024-12-22 18:29:02 +0800

    Update 2305_x64_test_diy-part2.sh

--- diy_script/lede_diy/x86/2305_x64_test_diy-part2.sh
+++ diy_script/lede_diy/x86/2305_x64_test_diy-part2.sh
@@ -57,6 +57,9 @@ if [ "$curl_ver" != "8.9.1" ]; then
     cp -rf $GITHUB_WORKSPACE/personal/curl feeds/packages/net/curl
 fi
 
+# 添加插件
+merge_package main https://github.com/gxnas/OpenWrt_Build_x64_Packages package/gxnas-packages luci-app-tailscale luci-app-poweroff
+
 # 报错修复
 rm -rf feeds/packages/utils/v2dat
 rm -rf package/feeds/packages/adguardhome
