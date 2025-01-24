52f4eb700e077b1fd82b6a2ba33ee8dbba763948
ranqingwen <76990231+ranqingwen@users.noreply.github.com>
2025-01-24 11:22:25 +0800

    Update 2305_x64_diy-part1.sh

--- diy_script/lede_diy/x86/2305_x64_diy-part1.sh
+++ diy_script/lede_diy/x86/2305_x64_diy-part1.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 #===============================================
 # Description: OpenWrt_Build_2305_x64 DIY script part 1
-# File name: 2305_x64_all_diy-part1.sh
+# File name: 2305_x64_diy-part1.sh
 # Lisence: MIT
-# By: GXNAS
+# By: ranqw
 #===============================================
 
 # Add a feed source
@@ -31,6 +31,7 @@ ver510=`grep "LINUX_VERSION-5.10 ="  include/kernel-5.10 | cut -d . -f 3`
 ver515=`grep "LINUX_VERSION-5.15 ="  include/kernel-5.15 | cut -d . -f 3`
 ver61=`grep "LINUX_VERSION-6.1 ="  include/kernel-6.1 | cut -d . -f 3`
 ver66=`grep "LINUX_VERSION-6.6 ="  include/kernel-6.6 | cut -d . -f 3`
+ver612=`grep "LINUX_VERSION-6.12 ="  include/kernel-6.12 | cut -d . -f 3`
 if [ "$str1" = "5.4" ];then
    mv  bin/targets/x86/64/openwrt-x86-64-generic-squashfs-combined.img.gz       bin/targets/x86/64/openwrt_x86-64_${str1}.${ver54}_bios.img.gz
    mv  bin/targets/x86/64/openwrt-x86-64-generic-squashfs-combined-efi.img.gz   bin/targets/x86/64/openwrt_x86-64_${str1}.${ver54}_uefi.img.gz
@@ -55,6 +56,14 @@ elif [ "$str1" = "6.6" ];then
   else
    mv  bin/targets/x86/64/openwrt-x86-64-generic-squashfs-combined.img.gz       bin/targets/x86/64/openwrt_x86-64_${str1}.${ver66}_bios.img.gz
    mv  bin/targets/x86/64/openwrt-x86-64-generic-squashfs-combined-efi.img.gz   bin/targets/x86/64/openwrt_x86-64_${str1}.${ver66}_uefi.img.gz
+   fi
+elif [ "$str1" = "6.12" ];then
+   if [ ! $ver612 ]; then
+   mv  bin/targets/x86/64/openwrt-x86-64-generic-squashfs-combined.img.gz       bin/targets/x86/64/openwrt_x86-64_${str1}.${ver612}_bios.img.gz
+   mv  bin/targets/x86/64/openwrt-x86-64-generic-squashfs-combined-efi.img.gz   bin/targets/x86/64/openwrt_x86-64_${str1}.${ver612}_uefi.img.gz
+  else
+   mv  bin/targets/x86/64/openwrt-x86-64-generic-squashfs-combined.img.gz       bin/targets/x86/64/openwrt_x86-64_${str1}.${ver612}_bios.img.gz
+   mv  bin/targets/x86/64/openwrt-x86-64-generic-squashfs-combined-efi.img.gz   bin/targets/x86/64/openwrt_x86-64_${str1}.${ver612}_uefi.img.gz
    fi
 fi
 #md5
